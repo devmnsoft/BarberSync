28033ed515486d2561a0f041ab73a008594d73c7
MNSOFT <137189378+devmnsoft@users.noreply.github.com>
2026-05-27 07:17:20 -0300

    Fix Admin dashboard layout/assets behavior and harden JS shell modules

--- Web/BarberSync.AdminWeb/wwwroot/js/admin-modal.js
+++ Web/BarberSync.AdminWeb/wwwroot/js/admin-modal.js
@@ -1,1 +1,8 @@
-// BarberSync admin script
+window.adminModal = {
+  open(id) {
+    document.getElementById(id)?.classList.add("is-open");
+  },
+  close(id) {
+    document.getElementById(id)?.classList.remove("is-open");
+  }
+};
